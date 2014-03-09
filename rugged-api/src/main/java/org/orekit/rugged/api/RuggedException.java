6a1118eab1765b15bd244cf21bc7042af1f4f58d
Luc Maisonobe <luc@orekit.org>
2014-03-09 11:13:22 +0100

    Added error messages handling, with translation.

---
+++ rugged-api/src/main/java/org/orekit/rugged/api/RuggedException.java
@@ -0,0 +1,192 @@
+/* Copyright 2002-2014 CS Systèmes d'Information
+ * Licensed to CS Systèmes d'Information (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.rugged.api;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+
+import org.apache.commons.math3.exception.util.ExceptionContext;
+import org.apache.commons.math3.exception.util.ExceptionContextProvider;
+import org.apache.commons.math3.exception.util.Localizable;
+
+/** This class is the base class for all specific exceptions thrown by
+ * the rugged library classes.
+
+ * <p>
+ * This class is heavily based on Orekit {@link org.orekit.errors.OrekitException},
+ * which is distributed under the terms of the Apache License V2.
+ * </p>
+ *
+ * @author Luc Maisonobe
+
+ */
+
+public class RuggedException extends Exception {
+
+    /** Serializable UID. */
+    private static final long serialVersionUID = 20140309L;
+
+    /** Exception context (may be null). */
+    private final ExceptionContext context;
+
+    /** Format specifier (to be translated). */
+    private final Localizable specifier;
+
+    /** Parts to insert in the format (no translation). */
+    private final Object[] parts;
+
+    /** Simple constructor.
+     * Build an exception with a translated and formatted message
+     * @param specifier format specifier (to be translated)
+     * @param parts parts to insert in the format (no translation)
+     */
+    public RuggedException(final Localizable specifier, final Object ... parts) {
+        this.context   = null;
+        this.specifier = specifier;
+        this.parts     = (parts == null) ? new Object[0] : parts.clone();
+    }
+
+    /** Copy constructor.
+     * @param exception exception to copy from
+     * @since 5.1
+     */
+    public RuggedException(final RuggedException exception) {
+        super(exception);
+        this.context   = exception.context;
+        this.specifier = exception.specifier;
+        this.parts     = exception.parts.clone();
+    }
+
+    /** Simple constructor.
+     * Build an exception from a cause and with a specified message
+     * @param message descriptive message
+     * @param cause underlying cause
+     */
+    public RuggedException(final Localizable message, final Throwable cause) {
+        super(cause);
+        this.context   = null;
+        this.specifier = message;
+        this.parts     = new Object[0];
+    }
+
+    /** Simple constructor.
+     * Build an exception from a cause and with a translated and formatted message
+     * @param cause underlying cause
+     * @param specifier format specifier (to be translated)
+     * @param parts parts to insert in the format (no translation)
+     */
+    public RuggedException(final Throwable cause, final Localizable specifier,
+                           final Object ... parts) {
+        super(cause);
+        this.context   = null;
+        this.specifier = specifier;
+        this.parts     = (parts == null) ? new Object[0] : parts.clone();
+    }
+
+    /** Simple constructor.
+     * Build an exception from an Apache Commons Math exception context context
+     * @param provider underlying exception context provider
+     */
+    public RuggedException(final ExceptionContextProvider provider) {
+        super(provider.getContext().getThrowable());
+        this.context   = provider.getContext();
+        this.specifier = null;
+        this.parts     = new Object[0];
+    }
+
+    /** Gets the message in a specified locale.
+     * @param locale Locale in which the message should be translated
+     * @return localized message
+     */
+    public String getMessage(final Locale locale) {
+        return (context != null) ?
+                context.getMessage(locale) :
+                buildMessage(locale, specifier, parts);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getMessage() {
+        return getMessage(Locale.US);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getLocalizedMessage() {
+        return getMessage(Locale.getDefault());
+    }
+
+    /** Get the localizable specifier of the error message.
+     * @return localizable specifier of the error message
+     */
+    public Localizable getSpecifier() {
+        return specifier;
+    }
+
+    /** Get the variable parts of the error message.
+     * @return a copy of the variable parts of the error message
+     */
+    public Object[] getParts() {
+        return parts.clone();
+    }
+
+    /**
+     * Builds a message string by from a pattern and its arguments.
+     * @param locale Locale in which the message should be translated
+     * @param specifier format specifier (to be translated)
+     * @param parts parts to insert in the format (no translation)
+     * @return a message string
+     */
+    private static String buildMessage(final Locale locale, final Localizable specifier,
+                                       final Object ... parts) {
+        return (specifier == null) ? "" : new MessageFormat(specifier.getLocalizedString(locale), locale).format(parts);
+    }
+
+    /** Create an {@link java.lang.RuntimeException} for an internal error.
+     * @param cause underlying cause
+     * @return an {@link java.lang.RuntimeException} for an internal error
+     */
+    public static RuntimeException createInternalError(final Throwable cause) {
+
+        /** Format specifier (to be translated). */
+        final Localizable specifier = RuggedMessages.INTERNAL_ERROR;
+
+        /** Parts to insert in the format (no translation). */
+        final String parts     = "orekit@c-s.fr";
+
+        return new RuntimeException() {
+
+            /** Serializable UID. */
+            private static final long serialVersionUID = 20140309L;
+
+            /** {@inheritDoc} */
+            @Override
+            public String getMessage() {
+                return buildMessage(Locale.US, specifier, parts);
+            }
+
+            /** {@inheritDoc} */
+            @Override
+            public String getLocalizedMessage() {
+                return buildMessage(Locale.getDefault(), specifier, parts);
+            }
+
+        };
+
+    }
+
+}
---
+++ rugged-api/src/test/java/org/orekit/rugged/api/RuggedMessagesTest.java
@@ -0,0 +1,104 @@
+/* Copyright 2002-2014 CS Systèmes d'Information
+ * Licensed to CS Systèmes d'Information (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.rugged.api;
+
+
+import java.text.MessageFormat;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RuggedMessagesTest {
+
+    @Test
+    public void testMessageNumber() {
+        Assert.assertEquals(2, RuggedMessages.values().length);
+    }
+
+    @Test
+    public void testAllKeysPresentInPropertiesFiles() {
+        for (final String language : new String[] { "en", "fr" } ) {
+            ResourceBundle bundle =
+                ResourceBundle.getBundle("assets/org/orekit/rugged/RuggedMessages",
+                                         new Locale(language), new RuggedMessages.UTF8Control());
+            for (RuggedMessages message : RuggedMessages.values()) {
+                final String messageKey = message.toString();
+                boolean keyPresent = false;
+                for (final Enumeration<String> keys = bundle.getKeys(); keys.hasMoreElements();) {
+                    keyPresent |= messageKey.equals(keys.nextElement());
+                }
+                Assert.assertTrue("missing key \"" + message.name() + "\" for language " + language,
+                                  keyPresent);
+            }
+            Assert.assertEquals(language, bundle.getLocale().getLanguage());
+        }
+
+    }
+
+    @Test
+    public void testAllPropertiesCorrespondToKeys() {
+        for (final String language : new String[] { "en", "fr" } ) {
+            ResourceBundle bundle =
+                ResourceBundle.getBundle("assets/org/orekit/rugged/RuggedMessages",
+                                         new Locale(language), new RuggedMessages.UTF8Control());
+            for (final Enumeration<String> keys = bundle.getKeys(); keys.hasMoreElements();) {
+                final String propertyKey = keys.nextElement();
+                try {
+                    Assert.assertNotNull(RuggedMessages.valueOf(propertyKey));
+                } catch (IllegalArgumentException iae) {
+                    Assert.fail("unknown key \"" + propertyKey + "\" in language " + language);
+                }
+            }
+            Assert.assertEquals(language, bundle.getLocale().getLanguage());
+        }
+
+    }
+
+    @Test
+    public void testNoMissingFrenchTranslation() {
+        for (RuggedMessages message : RuggedMessages.values()) {
+            String translated = message.getLocalizedString(Locale.FRENCH);
+            Assert.assertFalse(message.name(), translated.toLowerCase().contains("missing translation"));
+        }
+    }
+
+    @Test
+    public void testNoOpEnglishTranslation() {
+        for (RuggedMessages message : RuggedMessages.values()) {
+            String translated = message.getLocalizedString(Locale.ENGLISH);
+            Assert.assertEquals(message.getSourceString(), translated);
+        }
+    }
+
+    @Test
+    public void testVariablePartsConsistency() {
+        for (final String language : new String[] { "en", "fr" } ) {
+            Locale locale = new Locale(language);
+            for (RuggedMessages message : RuggedMessages.values()) {
+                MessageFormat source     = new MessageFormat(message.getSourceString());
+                MessageFormat translated = new MessageFormat(message.getLocalizedString(locale));
+                Assert.assertEquals(message.name() + " (" + language + ")",
+                                    source.getFormatsByArgumentIndex().length,
+                                    translated.getFormatsByArgumentIndex().length);
+            }
+        }
+    }
+
+}
