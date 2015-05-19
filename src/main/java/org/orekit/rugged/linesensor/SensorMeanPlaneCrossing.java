6c8742ea3dd3d75f9a37de0ca4fe973b925ffe95
Luc Maisonobe <luc@orekit.org>
2015-05-19 10:50:01 +0200

    Fixed Newton evaluation.

--- src/main/java/org/orekit/rugged/linesensor/SensorMeanPlaneCrossing.java
+++ src/main/java/org/orekit/rugged/linesensor/SensorMeanPlaneCrossing.java
@@ -534,9 +534,9 @@ public class SensorMeanPlaneCrossing {
                                                  (fMaxLine.getValue() - fMinLine.getValue());
             final double newtonExpectedLine;
             if (linearExpectedLine < midLine) {
-                newtonExpectedLine = minLine - fMinLine.getPartialDerivative(1) / fMinLine.getValue();
+                newtonExpectedLine = minLine - fMinLine.getValue() / fMinLine.getPartialDerivative(1);
             } else {
-                newtonExpectedLine = maxLine - fMaxLine.getPartialDerivative(1) / fMaxLine.getValue();
+                newtonExpectedLine = maxLine - fMaxLine.getValue() / fMaxLine.getPartialDerivative(1);
             }
             throw new InverseLocOutOfLineRangeException(newtonExpectedLine, minLine, maxLine);
         } catch (RuggedExceptionWrapper rew) {
