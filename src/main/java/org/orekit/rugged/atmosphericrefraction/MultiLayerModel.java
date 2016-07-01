3b78ccfd715a34a6ece0f3d6e4d00829116f0f70
sesteves <sroesteves@gmail.com>
2016-07-01 20:35:57 +0100

    minor

--- src/main/java/org/orekit/rugged/atmosphericrefraction/MultiLayerModel.java
+++ src/main/java/org/orekit/rugged/atmosphericrefraction/MultiLayerModel.java
@@ -8,8 +8,22 @@ import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
  */
 public class MultiLayerModel implements AtmosphericRefraction {
 
+    private static final double KARMA_LINE = 1000000;
+    private static final double LAYER_SIZE = KARMA_LINE * 0.25;
+
+    private int numberOfLayers = 1;
+
+    public MultiLayerModel() {
+    }
+
+    public MultiLayerModel(int numberOfLayers) {
+        this.numberOfLayers = numberOfLayers;
+    }
+
     @Override
     public long getDeviation(Vector3D pos, Vector3D los, double altitude) {
+
+        double numberOfCrossedLayers = (KARMA_LINE - altitude) / LAYER_SIZE;
 
 
 
