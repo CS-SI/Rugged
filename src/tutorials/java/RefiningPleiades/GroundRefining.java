bea9870702c9ada685c0276f801b504fde1b1fc1
Jonathan Guinet <jonathan.guinet@c-s.fr>
2017-07-21 10:31:16 -0400

    Affinage renamed Refining

--- src/tutorials/java/RefiningPleiades/GroundRefining.java
+++ src/tutorials/java/RefiningPleiades/GroundRefining.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package AffinagePleiades;
+package RefiningPleiades;
 
 import java.io.File;
 import java.util.List;
