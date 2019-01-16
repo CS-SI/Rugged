0dd6db3ac64f12d779ac40e14e185aae0dc89910
Guylaine Prat <guylaine.prat@c-s.fr>
2019-01-16 11:47:47 +0100

    Update Copyright year

--- src/test/java/org/orekit/rugged/api/RuggedBuilderTest.java
+++ src/test/java/org/orekit/rugged/api/RuggedBuilderTest.java
@@ -1,4 +1,4 @@
-/* Copyright 2013-2017 CS Systèmes d'Information
+/* Copyright 2013-2019 CS Systèmes d'Information
  * Licensed to CS Systèmes d'Information (CS) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
